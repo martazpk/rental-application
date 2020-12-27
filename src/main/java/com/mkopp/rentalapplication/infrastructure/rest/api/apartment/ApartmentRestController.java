737bfff0aeac54598aa7168ba639c60194268ff9
martazpk <kopp.marta@gmail.com>
2020-12-27 21:18:33 +0100

    adding new Apartment- expose rest controller

---
+++ src/main/java/com/mkopp/rentalapplication/infrastructure/rest/api/apartment/ApartmentRestController.java
@@ -0,0 +1,24 @@
+package com.mkopp.rentalapplication.infrastructure.rest.api.apartment;
+
+import com.mkopp.rentalapplication.application.apartment.ApartmentApplicationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/apartment")
+public class ApartmentRestController {
+    private final ApartmentApplicationService apartmentApplicationService;
+
+    public ApartmentRestController(ApartmentApplicationService apartmentApplicationService) {
+        this.apartmentApplicationService = apartmentApplicationService;
+    }
+
+    @PostMapping
+    public void add(@RequestBody ApartmentDto apartmentDto) {
+        apartmentApplicationService.add(apartmentDto.getOwnerId(), apartmentDto.getStreet(), apartmentDto.getPostalCode(),
+                apartmentDto.getHouseNumber(), apartmentDto.getApartmentNumber(), apartmentDto.getCity(), apartmentDto.getCountry(),
+                apartmentDto.getDescription(), apartmentDto.getRoomsDefinition());
+    }
+}
