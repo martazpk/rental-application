7f5c080eb65e53f00030366f3488b169bdb55065
martazpk <kopp.marta@gmail.com>
2020-12-29 11:20:12 +0100

    add history about apartment booking- impl event channel

---
+++ src/main/java/com/mkopp/rentalapplication/infrastructure/eventchannel/spring/SpringEventChannel.java
@@ -0,0 +1,25 @@
+package com.mkopp.rentalapplication.infrastructure.eventchannel.spring;
+
+import com.mkopp.rentalapplication.domain.apartment.ApartmentBooked;
+import com.mkopp.rentalapplication.domain.eventchannel.EventChannel;
+import com.mkopp.rentalapplication.domain.hotelRoom.HotelRoomBooked;
+import org.springframework.context.ApplicationEventPublisher;
+
+public class SpringEventChannel implements EventChannel {
+    private final ApplicationEventPublisher publisher;
+
+    public SpringEventChannel(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public void publish(ApartmentBooked apartmentBooked) {
+        publisher.publishEvent(apartmentBooked);
+
+    }
+
+    @Override
+    public void publish(HotelRoomBooked hotelRoomBooked) {
+        publisher.publishEvent(hotelRoomBooked);
+    }
+}
